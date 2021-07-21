1d952b2f954768f6d375291b5b13d42019f93a32
Rajdeep Sarkar <rajdeepsarkar1d@gmail.com>
2021-07-21 22:36:44 +0530

    db host error fixed

--- db/database.js
+++ db/database.js
@@ -3,10 +3,11 @@ const  Sequelize  = require('sequelize');
 const dbname = process.env.DBNAME 
 const username = process.env.DBUSERNAME 
 const password = process.env.DBPASSWORD
+const host = process.env.DBHOST
 
 
 const db = new Sequelize(dbname, username, password, {
-    host: 'localhost',
+    host: host,
     dialect:'postgres'
 });
 
